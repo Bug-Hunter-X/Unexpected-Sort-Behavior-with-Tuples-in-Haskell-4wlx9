1f79cb469885a1bd047509aa240beed27618b2fd
BugHunter-X <askzenai@gmail.com>
2025-01-10 21:18:08 +0100

    Add bugSolution.hs file

---
+++ bugSolution.hs
@@ -0,0 +1,22 @@
+```haskell
+import Data.List (sortWith)
+import Data.Ord (comparing)
+
+main :: IO ()
+main = do
+  let nums = [1, 5, 2, 8, 3]
+  let sortedNums = sort nums
+  print sortedNums -- This will print [1,2,3,5,8]
+
+  let xs = ["apple", "banana", "cherry"]
+  let sortedXs = sort xs
+  print sortedXs  -- This will print ["apple","banana","cherry"]
+
+  let ys = [(1, "a"), (3, "c"), (2, "b")]
+  let sortedYs = sortWith (comparing fst) ys
+  print sortedYs -- This will print [(1,"a"),(2,"b"),(3,"c")]
+
+  let zs = [(1, "c"), (3, "a"), (2, "b")]
+  let sortedZs = sortWith (comparing fst) zs
+  print sortedZs -- This will print [(1,"c"),(2,"b"),(3,"a")]
+```
